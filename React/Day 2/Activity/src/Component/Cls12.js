4d3c11429a69b3c66d326ad0803a63df33dfc172
kruthag <125258241+kruthag@users.noreply.github.com>
2023-11-18 12:27:59 +0530

    Add files via upload

---
+++ React/Day 2/Activity/src/Component/Cls12.js
@@ -0,0 +1,12 @@
+const C2=()=>
+{
+    return(
+        <>
+        <h1 style={{color:'green'}}>Inline Style in JSX Example</h1>
+        <div style={{backgroundColor:'lightblue',color:'darkblue',padding:'10px',border:'1px solid blue',borderRadius:'5px'}}>
+            <p style={{color:'darkblue', fontSize:'16px'}}>This paragraph within inline styles applied</p>
+        </div>
+        </>
+    )
+}
+export default C2;
